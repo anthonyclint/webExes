e0fa2da68aa78c3fff79495e2bf09a210f80d049
Anthony Clint Pastorello <dev.anthonyclint@gmail.com>
2022-11-06 21:20:42 -0300

    Adicionando efeito fade out à exclusão.

--- exe1/js/remove-paciente.js
+++ exe1/js/remove-paciente.js
@@ -1,7 +1,10 @@
-var pacientes = document.querySelectorAll(".paciente");
-
 var tabela = document.querySelector("#tabela-pacientes");
 
-tabela.addEventListener("dblclick", function(){
-    event.target.parentNode.remove(); //identificar na tabela quem recebeu o duplo clique, porém não eliminar apenas esse elemento
-})                                    //mas a linha inteira, por isso parentNode.
+tabela.addEventListener("dblclick", function(event){
+    event.target.parentNode.classList.add("fadeOut");
+
+    setTimeout(function(){
+        event.target.parentNode.remove(); //identificar na tabela quem recebeu o duplo clique, porém não eliminar apenas esse elemento
+                                          //mas a linha inteira, por isso parentNode.
+    },500); 
+});
